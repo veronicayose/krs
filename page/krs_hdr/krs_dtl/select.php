e331d082abe6c940d7035b556832dea03cecebf3
Veronica Yose Ardilla <veronica.ardilla@student.matanauniversity.ac.id>
2022-10-18 10:01:41 +0700

    update

--- page/krs_hdr/krs_dtl/select.php
+++ page/krs_hdr/krs_dtl/select.php
@@ -1,3 +1,4 @@
+
 <?php 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $statement = $mysqli->prepare("INSERT INTO krs_dtl (no_jadwal_detail, id_krs_hdr) VALUES (?, ?)");
@@ -10,13 +11,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 else {
     $result = $mysqli->query("SELECT * FROM jadwal_hdr a JOIN jadwal_dtl b ON a.no_jadwal_hdr=b.no_jadwal_hdr JOIN kelas c ON  c.kode_kelas = b.kode_kelas JOIN dosen d ON c.nid_dosen = d.nid_dosen JOIN matkul e ON e.kode_matkul = c.kode_matkul");
 ?>
-    <div class="table-responsive text-nowrap">
-    <form method="post">
-        <label for="basic-default-name">ID KRS Header</label>
-        <div>
-            <input value="<?= $_GET['id_krs_hdr']?>" type="number" class="form-control" id="basic-default-name" placeholder="ID KRS Header" name="id_krs_hdr" disabled>
-        </div>
-        <table class="table">
+<div class="card mb-4">
+    <div class="card-header d-flex align-items-center justify-content-between">
+        <h5 class="mb-0">Tambah KRS Details</h5>
+    </div>
+    <div class="card-body">
+    <table class="table">
             <thead>
                 <tr>
                     <th></th>
@@ -35,7 +35,11 @@ else {
                 ?>
                 <tr>
                     <td>
-                        <input value="<?= $row['no_jadwal_detail']?>" type="checkbox" class="form-control form-check-input" name="no_jadwal_detail">
+                        <form method="post">
+                            <input value="<?= $_GET['id_krs_hdr']?>" class="col-sm-2" name="id_krs_hdr" type="hidden"></input>
+                            <input value="<?= $row['no_jadwal_detail']?>" class="col-sm-2" name="no_jadwal_detail" type="hidden"></input>
+                        <button type="submit" class="btn btn-primary">Select</button>
+                        </form>
                     </td>
                     <td> <?=$row['semester'] ?></td>
                     <td> <?=$row['kode_ruang'] ?></td>
@@ -47,14 +51,13 @@ else {
                 </tr>
                 <?php }
                 ?>
+
             </tbody>
         </table>
-            <div class="row justify-content-start">
-                <div class="col-sm-10">
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </div>
-            </div>
-        </form>
+
+
+
+        
     </div>
 </div>
 <?php }
